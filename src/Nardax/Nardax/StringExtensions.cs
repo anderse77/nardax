0bd57a278ade26353dea61bf9a3a05503cbec820
DESKTOP-N6UL53L\Anders <anderse77@gmail.com>
2016-08-22 20:33:56 +0200

    Go bananas again.

--- src/Nardax/Nardax/StringExtensions.cs
+++ src/Nardax/Nardax/StringExtensions.cs
@@ -7,17 +7,7 @@ namespace Nardax
     {
         //Klipp av en sträng från vänster.
 
-        public static string TruncateLeft(this string value, int maxLength)
-        {
-            var startIndex = value.Length - maxLength;
-
-            if (startIndex < 1)
-            {
-                return value;
-            }
-
-            return value.Substring(startIndex, maxLength);
-        }
+        public static string TruncateLeftzzzzzzzzzzzzzzzzzzzzzzzz
 
         //Klipp av en sträng från höger.
 
