96301614380d0766270179421e3a505fc8ce0171
DESKTOP-N6UL53L\Anders <anderse77@gmail.com>
2016-08-25 22:37:08 +0200

    Korrigerande commits

--- src/Nardax.UnitTests/TruncateStringExtensionsTests.cs
+++ src/Nardax.UnitTests/TruncateStringExtensionsTests.cs
@@ -25,6 +25,16 @@ namespace Nardax.Tests
         }
 
         [TestMethod]
+        public void TruncateLeft_ValueIsLarger_TruncateRemovesLeftSide_KalleKulaTestar()
+        {
+            var expected = "789";
+
+            var result = _value.TruncateLeft(3);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
         public void TruncateLeft_ValueIsSmaller_TruncateRemovesLeftSide()
         {
             var expected = "123456789";
@@ -54,84 +64,5 @@ namespace Nardax.Tests
             Assert.AreEqual(expected, result);
         }
 
-        [TestMethod]
-        public void TruncateRight_ValueIsLarger_TruncateRemovesRightSide()
-        {
-            var expected = "1";
-
-            var result = _value.TruncateRight(1);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateRight_ValueIsSmaller_TruncateRemovesRightSide()
-        {
-            var expected = "123456789";
-
-            var result = _value.TruncateRight(20);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateRight_ValueIsLarger_TruncateRemovesRightSide()
-        {
-            var expected = "1";
-
-            var result = _value.TruncateRight(1);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateRight_ValueIsSmaller_TruncateRemovesRightSide()
-        {
-            var expected = "123456789";
-
-            var result = _value.TruncateRight(20);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateRight_ValueIsLarger_TruncateRemovesRightSide()
-        {
-            var expected = "1";
-
-            var result = _value.TruncateRight(1);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateRight_ValueIsSmaller_TruncateRemovesRightSide()
-        {
-            var expected = "123456789";
-
-            var result = _value.TruncateRight(20);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateRight_ValueIsLarger_TruncateRemovesRightSide()
-        {
-            var expected = "1";
-
-            var result = _value.TruncateRight(1);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateRight_ValueIsSmaller_TruncateRemovesRightSide()
-        {
-            var expected = "123456789";
-
-            var result = _value.TruncateRight(20);
-
-            Assert.AreEqual(expected, result);
-        }
     }
 }
